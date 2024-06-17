24dad70dee78aa551aa5a42329a027af3c7e658d
BFKHUser <tcsill770815@gmail.com>
2024-06-17 23:37:16 +0200

    progi

--- java/hu/gde/aycbph/entity/Story.java
+++ java/hu/gde/aycbph/entity/Story.java
@@ -64,4 +64,10 @@ public class Story {
         this.id = id;
     }
 
+    @Override
+    public String toString() {
+        return "Story{" +
+                "title='" + title + '\'' +
+                '}';
+    }
 }
